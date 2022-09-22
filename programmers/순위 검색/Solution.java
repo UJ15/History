7e60953bb2e4982a708c74ea47a5b5c7eca4af30
UJ15 <dbwogml15@naver.com>
2022-09-22 22:42:43 +0900

    ss

---
+++ programmers/순위 검색/Solution.java
@@ -0,0 +1,8 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/72412
+
+class Solutions {
+  public int[] solution(String[] info, String[] query) {
+    int[] answer = {};
+    return answer;
+  }
+}
