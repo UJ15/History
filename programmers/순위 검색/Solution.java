8b7659a717d6bdcf311d6f66d331745f82b7e8c8
UJ15 <dbwogml15@naver.com>
2022-09-25 22:18:01 +0900

    ss

--- programmers/순위 검색/Solution.java
+++ programmers/순위 검색/Solution.java
@@ -5,4 +5,20 @@ class Solutions {
     int[] answer = {};
     return answer;
   }
+
+  private permutation() {
+    if(depth == r){
+      for(int num: out) System.out.print(num);
+      System.out.println();
+      return;
+    }
+    for(int i=0; i<arr.length; i++){
+      if(!visited[i]){
+        visited[i] = true;
+        out[depth] = arr[i];
+        permutation(arr, out, visited, depth+1, r);
+        visited[i] = false;
+      }
+    }
+  }
 }
