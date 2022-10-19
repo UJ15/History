6bffa1b3320c11618820348a7cf7119a1a8162af
UJ15 <dbwogml15@naver.com>
2022-10-19 21:07:41 +0900

    problem solved

---
+++ programmers/예상대진표/Solution.java
@@ -0,0 +1,31 @@
+https://school.programmers.co.kr/learn/courses/30/lessons/12985
+
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int a, int b){
+        int answer = 1;
+        int left = 0;
+        int right = 0;
+
+        if(a > b){
+            left = b;
+            right = a;
+        } else {
+            left = a;
+            right = b;
+        }
+
+        while(true){
+            if(left % 2 != 0 && right - left == 1){
+                break;
+            }
+
+            left = (left + 1) / 2;
+            right = (right + 1) / 2;
+            answer++;
+        }
+
+        return answer;
+    }
+}
