d0764c3af5928cff8788e4697ccb449d52097975
UJ15 <dbwogml15@naver.com>
2023-01-20 12:57:58 +0900

    problem solved

---
+++ PS/programmers/폰켓몬/SecondSolved.java
@@ -0,0 +1,18 @@
+
+public class Solution {
+    public int solution(int[] nums) {
+        int max = nums.length / 2;
+
+        HashSet<Integer> numsSet = new HashSet<>();
+
+        for (int num : nums) {
+            numsSet.add(num);
+        }
+
+        if (numsSet.size() > max) {
+            return max;
+        }
+
+        return numsSet.size();
+    }
+}
