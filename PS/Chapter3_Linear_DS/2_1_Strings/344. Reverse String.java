409044c7abb09abbc4476f18d37542f5b22c42dd
UJ15 <dbwogml15@naver.com>
2023-01-24 15:27:21 +0900

    problem solved

--- PS/Chapter3_Linear_DS/2_1_Strings/344. Reverse String.java
+++ PS/Chapter3_Linear_DS/2_1_Strings/344. Reverse String.java
@@ -15,3 +15,19 @@ class Solution {
         }
     }
 }
+
+//book's solution
+/*import java.util.*;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int j = s.length - 1;
+
+        for (int i = 0 ; i < s.length / 2 ; i++) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            j--;
+        }
+    }
+}*/
