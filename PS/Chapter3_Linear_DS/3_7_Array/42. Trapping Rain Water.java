526587143398ff06a5b5285a52dc99dada8f2bb5
UJ15 <dbwogml15@naver.com>
2023-01-27 10:49:25 +0900

    problem solved

--- PS/Chapter3_Linear_DS/3_7_Array/42. Trapping Rain Water.java
+++ PS/Chapter3_Linear_DS/3_7_Array/42. Trapping Rain Water.java
@@ -1,27 +1,27 @@
-
-
-class Solution {
-    public int trap(int[] height) {
-        int water = 0;
-        int left = 0;
-        int right = height.length - 1;
-
-        int leftHigh = height[left];
-        int rightHigh = height[right];
-
-        while (left < right) {
-            leftHigh = Math.max(leftHigh, height[left]);
-            rightHigh = Math.max(rightHigh, height[right]);
-
-            if (leftHigh < rightHigh) {
-                water += leftHigh - height[left];
-                left++;
-            } else {
-                water += rightHigh - height[right];
-                right--;
-            }
-        }
-
-        return water;
-    }
-}
+//
+//
+//class Solution {
+//    public int trap(int[] height) {
+//        int water = 0;
+//        int left = 0;
+//        int right = height.length - 1;
+//
+//        int leftHigh = height[left];
+//        int rightHigh = height[right];
+//
+//        while (left < right) {
+//            leftHigh = Math.max(leftHigh, height[left]);
+//            rightHigh = Math.max(rightHigh, height[right]);
+//
+//            if (leftHigh < rightHigh) {
+//                water += leftHigh - height[left];
+//                left++;
+//            } else {
+//                water += rightHigh - height[right];
+//                right--;
+//            }
+//        }
+//
+//        return water;
+//    }
+//}
---
+++ PS/leetcode/3_7_Array/15. 3Sum.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+
+        for (int i = 0 ; i < nums.length - 2 ; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    list.add(List.of(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left > right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return list;
+    }
+}
