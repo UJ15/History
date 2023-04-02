ecbbf3c785cccfe7978a9c0c91052fab14a6b70e
UJ15 <dbwogml15@naver.com>
2023-04-02 11:00:32 +0900

    refactor

--- PS/leetcode/3_13_Tree/543. Diameter of Binary Tree.java
+++ PS/leetcode/3_13_Tree/543. Diameter of Binary Tree.java
@@ -17,36 +17,20 @@ class Solution {
     private int distance;
 
     public int diameterOfBinaryTree(TreeNode root) {
-
         dfs(root);
+
         return distance;
     }
 
     private int dfs(TreeNode root) {
-        if (root.left == null && root.right == null) {
+        if (root == null) {
             return 0;
         }
 
-        int left = 0;
-        int right = 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
 
-        if (root.left == null) {
-            left -= 1;
-        }
-
-        if (root.right == null) {
-            right -= 1;
-        }
-
-        if (root.left != null) {
-            left = dfs(root.left);
-        }
-
-        if (root.right != null) {
-            right = dfs(root.right);
-        }
-
-        distance = Math.max(distance, left + right + 2);
+        distance = Math.max(distance, left + right);
 
         return Math.max(left, right) + 1;
     }
