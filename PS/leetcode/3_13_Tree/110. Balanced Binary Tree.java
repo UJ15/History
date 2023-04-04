3d6114b4ba17fe92213820217d0b263b3ab327f6
UJ15 <dbwogml15@naver.com>
2023-04-04 15:21:26 +0900

    problem solved

---
+++ PS/leetcode/3_13_Tree/110. Balanced Binary Tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean flag;
+
+    public boolean isBalanced(TreeNode root) {
+        flag = true;
+
+        dfs(root);
+        return flag;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if (Math.abs(left - right) > 1) {
+            flag = false;
+        }
+
+        return Math.max(left, right) + 1;
+    }
+}
