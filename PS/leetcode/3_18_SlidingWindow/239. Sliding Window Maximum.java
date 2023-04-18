6e88e9185223a135fcc75619e4046e8fc43aa018
UJ15 <dbwogml15@naver.com>
2023-04-18 22:03:44 +0900

    problem solved

--- PS/leetcode/3_18_SlidingWindow/239. Sliding Window Maximum.java
+++ PS/leetcode/3_18_SlidingWindow/239. Sliding Window Maximum.java
@@ -1,25 +1,26 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> q = new ArrayDeque<>();
-        int[] ans = new int[nums.length - k + 1];
-        int j = -1;
+        int[] result = new int[nums.length - k + 1];
+        int j = 0;
 
-        for(int i = 0 ; i < nums.length ; i++) {
+        for (int i = 0 ; i < nums.length ; i++) {
 
             if (!q.isEmpty() && q.getFirst() == i - k) {
                 q.removeFirst();
             }
 
-            while (!q.isEmpty() && nums[q.getLast()]<nums[i]) {
+            while (!q.isEmpty() && nums[q.getLast()] < nums[i]) {
                 q.removeLast();
             }
 
             q.addLast(i);
 
             if (i >= k - 1) {
-                ans[++j]=nums[q.getFirst()];
+                result[j++] = nums[q.getFirst()];
             }
         }
-        return ans;
+
+        return result;
     }
 }
