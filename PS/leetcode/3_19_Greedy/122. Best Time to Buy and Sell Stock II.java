b6ec4669151befcda058a62e5cce01185304cff4
UJ15 <dbwogml15@naver.com>
2023-04-20 14:32:51 +0900

    problem solved

--- PS/leetcode/3_19_Greedy/122. Best Time to Buy and Sell Stock II.java
+++ PS/leetcode/3_19_Greedy/122. Best Time to Buy and Sell Stock II.java
@@ -15,3 +15,21 @@ class Solution {
         return profit;
     }
 }
+
+/**
+ * Greedy
+ */
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+
+        for (int i = 0 ; i < prices.length - 1 ; i++) {
+            if (prices[i + 1] > prices[i]) {
+                result += prices[i + 1] - prices[i];
+            }
+        }
+
+        return result;
+    }
+}
