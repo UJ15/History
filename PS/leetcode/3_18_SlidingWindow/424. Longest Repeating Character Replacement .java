e0975c035941047757ddd2ffabb0e1843a80c3f1
UJ15 <dbwogml15@naver.com>
2023-04-20 11:06:25 +0900

    problem solved

--- PS/leetcode/3_18_SlidingWindow/424. Longest Repeating Character Replacement .java
+++ PS/leetcode/3_18_SlidingWindow/424. Longest Repeating Character Replacement .java
@@ -16,3 +16,34 @@ class Solution {
         return result;
     }
 }
+
+/**
+ * 내가 시도한 방법
+ * - 배열 쓰면 되는데 굳이 해시맵 쓰려는 버릇을 고쳐보자
+ *
+ */
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLen = -1;
+        int result = 0;
+
+        for (int i = 0 ; i < s.length() ; i++) {
+            map.put(s.charAt(i), 0);
+        }
+
+        for (int i = 0 ; i < s.length() ; i++) {
+            map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            maxLen = Math.max(maxLen, map.get(s.charAt(i)));
+
+            if (result - maxLen < k) {
+                result++;
+            } else {
+                map.put(s.charAt(i - result), map.get(s.charAt(i - result)) - 1);
+            }
+        }
+
+        return result;
+    }
+}
