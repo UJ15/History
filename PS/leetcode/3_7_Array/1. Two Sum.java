b3412289d3c444a85159ce3c99b4fc46b22f703a
UJ15 <dbwogml15@naver.com>
2023-04-30 16:17:39 +0900

    problem solved

--- PS/leetcode/3_7_Array/1. Two Sum.java
+++ PS/leetcode/3_7_Array/1. Two Sum.java
@@ -11,3 +11,26 @@ class Solution {
         return null;
     }
 }
+
+//더 빠른 풀이
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int index = 0;
+
+        for (int num : nums) {
+            map.put(num, index);
+            index++;
+        }
+
+        for (int i = 0 ; i < nums.length ; i++) {
+            int t = target - nums[i];
+
+            if (map.containsKey(t) && map.get(t) != i) {
+                return new int[] {i, map.get(t)};
+            }
+        }
+
+        return null;
+    }
+}
