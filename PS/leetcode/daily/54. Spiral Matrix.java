47d2d83ebc1e68e94ede3965ed0e041c5f948dba
UJ15 <dbwogml15@naver.com>
2023-05-09 11:59:00 +0900

    problem solved

---
+++ PS/leetcode/daily/54. Spiral Matrix.java
@@ -0,0 +1,48 @@
+/***
+ * Queue를 이용해서 BFS처럼 풀었다.
+ * 방향전환을 반복하면서 불가능한 인덱스일경우 리턴
+ */
+
+
+class Solution {
+    List<Integer> result;
+    int direction;
+    boolean[][] visited;
+    int[] dx = {0, 1, 0, -1};
+    int[] dy = {1, 0, -1, 0};
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        direction = 0;
+        int length = matrix.length;
+        int height = matrix[0].length;
+
+        visited = new boolean[length][height];
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {0, 0});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            visited[cur[0]][cur[1]] = true;
+            result.add(matrix[cur[0]][cur[1]]);
+
+            int nextX = cur[0] + dx[direction];
+            int nextY = cur[1] + dy[direction];
+
+            if (nextX < 0 || nextY < 0 || nextX == length || nextY == height || visited[nextX][nextY]) {
+                direction = (direction + 1) % 4;
+                nextX = cur[0] + dx[direction];
+                nextY = cur[1] + dy[direction];
+            }
+
+            if (nextX < 0 || nextY < 0 || nextX == length || nextY == height || visited[nextX][nextY]) {
+                break;
+            }
+
+            q.offer(new int[] {nextX, nextY});
+        }
+
+        return result;
+    }
+}
