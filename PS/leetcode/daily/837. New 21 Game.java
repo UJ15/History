5178e8b602dba9e405c45501a59651f4ac26957e
UJ15 <dbwogml15@naver.com>
2023-05-25 19:33:58 +0900

    problem solved

---
+++ PS/leetcode/daily/837. New 21 Game.java
@@ -0,0 +1,34 @@
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+        if (k == 0) {
+            return 1.0;
+        }
+
+        if (n >= k - 1 + maxPts) {
+            return 1.0;
+        }
+
+        double[] dp = new double[n + 1];
+        Arrays.fill(dp, 0.0);
+
+        double probability = 0.0;
+        double windowSum = 1.0;
+        dp[0] = 1.0;
+
+        for (int i = 1 ; i <= n ; i++) {
+            dp[i] = windowSum / maxPts;
+
+            if (i < k) {
+                windowSum += dp[i];
+            } else {
+                probability += dp[i];
+            }
+
+            if (i >= maxPts) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+
+        return probability;
+    }
+}
