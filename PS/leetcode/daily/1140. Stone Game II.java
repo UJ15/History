e7cb527d0d200e43a7106b3dd7de0ea94e2ea52b
UJ15 <dbwogml15@naver.com>
2023-05-26 09:39:08 +0900

    problem solved

---
+++ PS/leetcode/daily/1140. Stone Game II.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int[][] d = new int[piles.length][piles.length];
+
+        int[] suffixSum = new int[piles.length];
+        suffixSum[suffixSum.length - 1] = piles[piles.length - 1];
+
+        for (int i = piles.length - 2 ; i >= 0 ; i--) {
+            suffixSum[i] = piles[i] + suffixSum[i + 1];
+        }
+
+        return dfs(piles, suffixSum, d, 0, 1);
+    }
+
+    private int dfs(int[] piles, int[] suffixSum, int[][] d, int firstPile, int M) {
+        if (firstPile == piles.length) {
+            return 0;
+        }
+
+        if (piles.length - firstPile <= 2 * M) {
+            return suffixSum[firstPile];
+        }
+
+        if (d[firstPile][M] != 0) {
+            return d[firstPile][M];
+        }
+
+        int result = 0;
+
+        for (int x = 1 ; x <= 2 * M ; x++) {
+            result = Math.max(result, suffixSum[firstPile] - dfs(piles, suffixSum, d, firstPile + x, Math.max(M, x)));
+        }
+
+        d[firstPile][M] = result;
+        return result;
+    }
+}
