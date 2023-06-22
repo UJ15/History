d94a19bc4596fc3f0985836264c30364f4984328
UJ15 <dbwogml15@naver.com>
2023-06-22 09:42:35 +0900

    solved problem

--- PS/leetcode/daily/703. Kth Largest Element in a Stream.java
+++ PS/leetcode/daily/703. Kth Largest Element in a Stream.java
@@ -28,3 +28,82 @@ class KthLargest {
  * KthLargest obj = new KthLargest(k, nums);
  * int param_1 = obj.add(val);
  */
+
+//Best Solution
+
+class KthLargest {
+    private int k;
+    private int size;
+    private int[] heap;
+
+    public KthLargest(int k, int[] nums) {
+        //Initial heap
+        this.k = k;
+        this.heap = nums;
+        this.size = nums.length;
+        //heapify TODO
+        for (int i = (nums.length - 1) / 2; i >= 0; i--) {
+            sink(i);
+        }
+        //Resize
+        while (size > k) {//delete excessive
+            del();
+        }
+        //Min heap of size k/k-1
+        this.size = Math.min(k, nums.length);
+        this.heap = Arrays.copyOf(nums, k);
+    }
+
+
+    void swim(int i) {
+        while (i != 0 && heap[(i - 1) / 2] > heap[i]) {
+            swap((i - 1) / 2, i);
+            i = (i - 1) / 2;
+        }
+    }
+
+    void sink(int i) {
+        while (2*i + 1 < size) {
+            int k = 2 * i + 1;
+            if (k + 1 < size && heap[k + 1] < heap[k]) {
+                k++;
+            }
+            if (heap[k] >= heap[i]) {
+                break;
+            }
+            swap(i, k);
+            i = k;
+        }
+    }
+
+    void del() {
+        swap(0, size - 1);
+        size--;
+        sink(0);
+    }
+
+    void swap(int i, int j) {
+        int tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+
+    public int add(int val) {
+        if (size != k) {
+            heap[heap.length - 1] = val;
+            size++;
+            swim(heap.length - 1);
+            return heap[0];
+        }
+        if (val > heap[0]) {
+            heap[0] = val;
+            sink(0);
+        }
+        return heap[0];
+    }
+}
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
