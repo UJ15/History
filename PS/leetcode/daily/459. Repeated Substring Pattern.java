cd6d4dbe3ffc212e0d467582eee226ea06b5dfbe
UJ15 <dbwogml15@naver.com>
2023-08-22 09:02:41 +0900

    solved problem

---
+++ PS/leetcode/daily/459. Repeated Substring Pattern.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String sub = doubled.substring(1, doubled.length() - 1);
+
+        return sub.contains(s);
+    }
+
+}
