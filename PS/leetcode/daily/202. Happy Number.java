b20f525fcd417a503ae492b11347e013321da230
UJ15 <dbwogml15@naver.com>
2023-09-20 23:53:03 +0900

    solved problem

---
+++ PS/leetcode/daily/202. Happy Number.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do{
+            slow=find(slow);
+            fast=find(find(fast));
+        }while(slow!=fast);
+        if(slow==1){
+            return true;
+        }
+        return false;
+    }
+    private int find(int num){
+        int ans=0;
+        while(num>0){
+            int rem=num%10;
+            ans+=rem*rem;
+            num/=10;
+        }
+        return ans;
+    }
+}
